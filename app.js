b7bf0b75e94f49eab9795bf8fe4864349596f0db
RD13p <vicrodriguez9913@gmail.com>
2021-09-12 17:04:15 -0500

    fix errors

--- app.js
+++ app.js
@@ -10,19 +10,20 @@ const app = express();
 const indexRoutes = require("./routes/routeindex");
 
 // Configuraciones
-app.set("port", config.app.port);
+var PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // Middleware  
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Rutas
 app.use("/", indexRoutes);
 
 // Listener
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
