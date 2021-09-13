624fd44bea96233cb1b1cd7129fa4922ff9492dd
RD13p <vicrodriguez9913@gmail.com>
2021-09-12 23:22:55 -0500

    ajustes a formato

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,13 @@ var validate = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const email = document.getElementById("email-input");
 var submitNewsletter = document.getElementById("submit-newsletter");
-var toastLiveExample = document.getElementById('liveToast')
-
 const database = firebase.database();
 
 submitNewsletter.addEventListener('click', (e) =>{
     e.preventDefault();
 
-    if(email.value == "" || (!email.value.match(validate))){
-        alert("Ingresa un correo electrónico válido");
-    } else {
-    let current = new Date();
+    if(email.value.match(validate)){
+        let current = new Date();
     let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
     let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
     let dateTime = cDate + ' ' + cTime;
@@ -20,6 +16,9 @@ submitNewsletter.addEventListener('click', (e) =>{
         email: email.value
     });
     alert("Registro exitoso 🧩");
+    } else {
+   alert("Ingresa un correo elctrónico válido");
+   email.focus();
 }
     email.value = '';
     
